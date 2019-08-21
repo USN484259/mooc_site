848bee7fac81679fd871633567e1b7ec1d1e66b8
USN484259 <USN484259@outlook.com>
2019-08-22 01:02:01 +0800

    registration

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path,include
-from django.contrib.auth import login,logout
 from . import views
 
+# from django.views.generic.edit import CreateView
+# from django.contrib.auth.forms import UserCreationForm
+
 urlpatterns=[
+    # path("register",CreateView.as_view(
+            # template_name='registration/register.html',
+            # form_class=UserCreationForm,
+            # success_url='/'
+    # ),name="register"),
     path("register",views.register,name="register"),
     path("",include('django.contrib.auth.urls')),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth.models import Group
 
 # Create your views here.
 
 def register(req):
-    return render(req,"registration/register.html")
+    if req.method=="POST":
+        form=UserCreationForm(req.POST)
+        if form.is_valid():
+            user=form.save()
+            group=Group.objects.get(name="student")
+            user.groups.add(group)
+            return redirect("/")
+    else:  
+        form=UserCreationForm
+        
+        
+    return render(req,"registration/register.html",{"form":form})
 
